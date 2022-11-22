274c862dd026db6b0be4f5d191ccfd3831b66015
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-11-22 12:47:17 -0300

    create for rides

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,4 +2,25 @@ class RidesController < ApplicationController
   def index
   end
 
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.new(rides_params)
+    puts(current_user.email)
+    @ride.user = current_user
+    if @ride.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rides_params
+    params.require(:ride).permit(:route, :date, :price, :pickup_address, :time, :passengers)
+  end
+
 end
