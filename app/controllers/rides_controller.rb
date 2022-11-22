e9bf67556dc434b6a8d76cbb7689a47dc586fcf6
lola martinhao <lola.martinhao@gmail.com>
2022-11-22 14:07:17 -0300

    adding index and show pages

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,22 @@
 class RidesController < ApplicationController
+  before_action :set_ride, only: %i[show]
+
   def index
+    @rides = Ride.all
   end
 
+  def show
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ride
+    @ride = Ride.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def ride_params
+    params.require(:ride).permit(:user, :route, :date, :price, :pickup_address, :time, :passengers)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :rides
+  root to: "rides#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
