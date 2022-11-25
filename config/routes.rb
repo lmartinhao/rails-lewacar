2c726245ecacee5b6a9b138f950c6de718483a1f
lola martinhao <lola.martinhao@gmail.com>
2022-11-25 15:08:06 -0300

    matches page finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :rides do
-    resources :matches, only: [:new, :create]
+    resources :matches, only: %i[new create]
   end
+
   root to: "rides#index"
+
+  get "reservations", to: "reservations#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
